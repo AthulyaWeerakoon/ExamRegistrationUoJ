9b5d4d1f28af0db2442f28ea0bafa0c71a931a44
Sachithra-oshadha <141540024+Sachithra-oshadha@users.noreply.github.com>
2024-05-26 18:25:21 +0530

    student registration class and interface v1

--- ExamRegistrationUoJ/PageClasses/StudentPages/StudentRegistration.cs
+++ ExamRegistrationUoJ/PageClasses/StudentPages/StudentRegistration.cs
@@ -1,7 +1,43 @@
+using ExamRegistrationUoJ.Services.DBInterfaces;
+using System.Collections;
+using System.Data;
+
 namespace StudentPages
 {
     public class StudentRegistration
     {
-        
+        private IDBServiceStudentRegistration db;
+        public DataTable? departments { get; set; }
+        public DataTable? semesters { get; set; }
+        public DataTable? students { get; set; }
+        private DataTable? allExams { get; set; }
+        private DataTable? examDept { get; set; }
+        public DataView? exams { get; set; }
+        public string departmentOpt { get; set; } = "Department";
+        public string semesterOpt { get; set; } = "Semester";
+        public string statusOpt { get; set; } = "Registration Status";
+        StudentRegistration(IDBServiceStudentRegistration db)
+        {
+            this.db = db;
+        }
+        public async Task getDepartments()
+        {
+            this.departments = await db.getDepartments();
+        }
+        public async Task getSemesters()
+        {
+            this.semesters = await db.getSemesters();
+        }
+
+        //get students from students table 
+        public async Task getStudents()
+        {
+            this.students = await db.getStudents();
+        }
+        //get exams from courses_in_exam table
+        public async Task getExams()
+        {
+            this.exams = await db.getExams();
+        }
     }
 }
---
+++ ExamRegistrationUoJ/Services/DBInterfaces/IDBServiceStudentRegistration.cs
@@ -0,0 +1,59 @@
+﻿using System.Data;
+
+namespace ExamRegistrationUoJ.Services.DBInterfaces
+{
+    public interface IDBServiceStudentRegistration
+    {
+        public Task<DataTable> getDepartments();
+        /*
+        Return structure for getDepartments
+
+        Name        Description         Type
+        id          Department id (pk)  uint
+        name        Department name     string
+        
+        Need details from all departments
+        */
+
+        public Task<DataTable> getSemesters();
+        /*
+        Return structure for getSemesters
+
+        Name        Description         Type
+        id          Semester id (pk)    uint
+        name        Semester name       string
+        
+        Need details from all semesters
+        */
+
+        public Task<DataTable> getStudents();
+        /*
+        parameters : user id/student id
+
+        Return structure for getStudents
+
+        Name        Description         Type
+        id          student id(pk)      uint
+        name        student name        string
+        
+        */
+
+        public Task<DataTable> getExams();
+        /*
+        parameters : ?
+
+        Return structure for getExams
+
+        Name            Description         Type
+        id              exam id             uint
+        course_id       course id           uint
+        course_name     course name         string
+        department_id   deparment id        uint
+        department_name department name     string
+        coordinator_id  coordinator id      uint
+        coordinator_name coordinator name   string
+        
+        Need details from all semesters
+        */
+    }
+}
