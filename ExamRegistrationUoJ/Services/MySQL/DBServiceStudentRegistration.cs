ead2952e28662f80ed6fa85bda97e978946fefb2
Sachithra-oshadha <141540024+Sachithra-oshadha@users.noreply.github.com>
2024-06-13 00:21:02 +0530

    Revert "hope this will work"
    This reverts commit f56a57c60a651468e4c2700e5916595862b58781.

--- ExamRegistrationUoJ/Services/MySQL/DBServiceStudentRegistration.cs
+++ ExamRegistrationUoJ/Services/MySQL/DBServiceStudentRegistration.cs
@@ -1,61 +1,15 @@
 ﻿// Ramith's workspace
 
 using ExamRegistrationUoJ.Services.DBInterfaces;
-using Microsoft.Identity.Client;
 using MySqlConnector;
 using System.Data;
 
 namespace ExamRegistrationUoJ.Services.MySQL
 {
-    public partial class DBMySQL : IDBServiceSR
+    public partial class DBMySQL : IDBServiceStudentRegistration
     {
-        public Task<DataTable> getAdvisors()
-        {
-            throw new NotImplementedException();
-        }
 
-        public async Task<DataTable> getCourses(ulong examId)
-        {
-            DataTable dataTable = new DataTable();
-
-            try
-            {
-                // Open the connection if it's not already open
-                if (_connection?.State != ConnectionState.Open)
-                    OpenConnection();
-
-                // SQL query to select courses based on the examId
-
-                string query = "SELECT  cie.id as id, c.name AS course_name, c.code AS course_code, a.ms_email AS coordinator_email, cie.department_id AS dep_id " +
-                               "FROM courses_in_exam cie " +
-                               "JOIN courses c ON cie.course_id = c.id " +
-                               "JOIN coordinators co ON cie.coordinator_id = co.id " +
-                               "JOIN accounts a ON co.account_id = a.id " +
-                               "WHERE cie.exam_id = @examID;";
-
-                // MySqlCommand to execute the SQL query
-                using (MySqlCommand cmd = new MySqlCommand(query, _connection))
-                {
-                    // Define the parameter and assign its value
-                    cmd.Parameters.AddWithValue("@examId", examId);
-
-                    // Execute the query and load the results into a DataTable
-                    using (MySqlDataReader reader = await cmd.ExecuteReaderAsync())
-                    {
-                        dataTable.Load(reader);
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Error: {ex.Message}");
-                throw;
-            }
-            return dataTable;
-        }
-
-
-        public async Task<DataTable> getStudent(ulong studentId)
+        public async Task<DataTable> getCourses()
         {
             DataTable dataTable = new DataTable();
 
@@ -66,14 +20,21 @@ namespace ExamRegistrationUoJ.Services.MySQL
                     OpenConnection();
 
                 // SQL query to select semester id and name from the semesters table
-                string query = "SELECT a.id, a.name, a.ms_email FROM accounts a WHERE a.id = (SELECT account_id FROM students WHERE id = @studentId); ";
+                string query = "SELECT c.id, " +
+                       "cie.department_id AS dep_id, " +
+                       "c.name, " +
+                       "a.name AS coordinator, " +
+                       "c.code " +
+                       "FROM courses_in_exam cie " +
+                       "JOIN courses c ON cie.course_id = c.id " +
+                       "JOIN departments d ON cie.department_id = d.id " +
+                       "JOIN coordinators co ON cie.coordinator_id = co.id " +
+                       "JOIN accounts a ON co.account_id = a.id " +
+                       "WHERE cie.exam_id = @examId";
 
                 // MySqlCommand to execute the SQL query
                 using (MySqlCommand cmd = new MySqlCommand(query, _connection))
                 {
-                    // Define the parameter and assign its value
-                    cmd.Parameters.AddWithValue("@studentId", studentId);
-
                     // Execute the query and load the results into a DataTable
                     using (MySqlDataReader reader = await cmd.ExecuteReaderAsync())
                     {
@@ -89,8 +50,14 @@ namespace ExamRegistrationUoJ.Services.MySQL
             return dataTable;
         }
 
-
-
+        Task<DataTable> IDBServiceStudentRegistration.getStudents()
+        {
+            throw new NotImplementedException();
+        }
     }
 
+
+
+
+    
 }
