3f1f30b1262569a3f7a39663b37571195f39d19c
2020 E 165 UPATHILAK A.L.A.A. <2020e165@eng.jfn.ac.lk>
2024-06-14 17:55:26 +0530

    Coordinator page 90% done

--- ExamRegistrationUoJ/Services/DBInterfaces/IDBServiceCoordinator1.cs
+++ ExamRegistrationUoJ/Services/DBInterfaces/IDBServiceCoordinator1.cs
@@ -9,7 +9,7 @@ namespace ExamRegistrationUoJ.Services.DBInterfaces
         /*    (Coordinator Home)
         Return structure for getDepartments
         Name        Description         Type
-        id          Department id (pk)  uint    
+        id          Department id (pk)  uint    S
         name        Department name     string
         Need details from all departments
         */
@@ -24,36 +24,17 @@ namespace ExamRegistrationUoJ.Services.DBInterfaces
         Need details from all semesters
         */
 
-        public Task<DataTable> getExams();
-        /* (Coordinator Home page table)
-        Return structure for getExams
-        Name        Description         Type
-        id          Exam id (pk)        uint
-        open_date   Open date           date
-        closed_date Closed date         date
-        name        Exam name           string
-        id          Semester id (pk)    uint
-        semester_id Semester ID         uint
-        semester    Semester Name       string
-        status      Is confirmed        uint
-        */
+        //practice to how to get email
+        public Task<int> GetCoordinatorID(string email);
 
-        public Task<DataTable> getExamAndStudent();
-        /* (Coordinator approve page)
-        Return structure for getExamAndStudent
-        Name        Description         Type
-        name        Student name        string
-        enumber     Student number(pk)  string
-        student_advisor Student advisor string
-        eligible    Is eligible         uint
-        */
+        public  Task<DataTable> getExamDept_coordinator(string email);
 
-        public Task<DataTable> getCorrdinatorCource();
-        /* (Coordinator approve page)
-        Return structure for getCorrdinatorCource
-        Name        Description         Type
-        course_id   Course ID           uint
-        course_name Course name         string
-        */
+        public Task<DataTable> getExamDetails_coordinator(string email);
+
+        public  Task<DataTable> getExamDetails_student(int exam_id);
+
+
+        //ramith's workspace
+        //public  Task<DataTable> getStudentDetails_in_Course(int exam_id, string course_id, int coordinator_id);
     }
 }
