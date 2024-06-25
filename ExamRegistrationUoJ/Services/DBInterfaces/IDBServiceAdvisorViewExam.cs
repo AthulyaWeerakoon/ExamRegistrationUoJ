7d17b31a6e52cf8b90212a7a66149667fe4bbd01
Sachithra-oshadha <141540024+Sachithra-oshadha@users.noreply.github.com>
2024-06-25 19:18:44 +0530

    Advisor view exam finished

--- ExamRegistrationUoJ/Services/DBInterfaces/IDBServiceAdvisorViewExam.cs
+++ ExamRegistrationUoJ/Services/DBInterfaces/IDBServiceAdvisorViewExam.cs
@@ -4,7 +4,9 @@ namespace ExamRegistrationUoJ.Services.DBInterfaces
 {
     public interface IDBServiceAdvisorViewExam
     {
-        public Task<DataTable> getStudent(uint studentId);
+        public Task<DataTable> getStudentsInExam(uint examId);
         public Task<DataTable> getExamTitle(uint examId);
+        //public Task<DataTable> test_a(uint examid);
+        public Task setAdvisorApproval(uint sieId, uint examId, bool isApproved);
     }
 }
